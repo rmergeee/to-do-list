78ad93fb34ce644cf605c6f9f34bf65ed7d4e6ba
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-10-03 21:51:46 +0300

    feat: add property "project" for Task

--- src/core/Task.js
+++ src/core/Task.js
@@ -1,8 +1,10 @@
 export default class Task {
-  constructor(title, description, deadline) {
+  constructor(title, description, deadline, project) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.description = description;
     this.deadline = deadline;
     this.status = true;
+    this.project = project;
   }
 }
