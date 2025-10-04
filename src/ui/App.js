12b982d37dce1d892c8bbe9955aca38bab8e488b
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-10-04 05:20:25 +0300

    feat: create a task structure + event listener for the checkbox

--- src/ui/App.js
+++ src/ui/App.js
@@ -5,22 +5,43 @@ export default class App {
 
     constructor() {
         this.todo = new ToDoList();
-        this.storage = new LocalStorageRepo();
+        this.storage = new LocalStorageRepo(this.todo);
+    }
+
+    addEvents() {
+        const checkboxes = document.querySelectorAll("input[name=taskStatus]");
+        checkboxes.forEach((checkbox) => {
+            checkbox.addEventListener("change", () => {
+                this.todo.changeStatus(checkbox.id);
+                console.log(this.todo.taskStorage.find(item => item.id === checkbox.id))
+            })
+
+        });
+
     }
 
     updatePage() {
-        this.storage.getAll(this.todo);
+        this.storage.getAll();
         for (let tsk of this.todo.taskStorage) {
+            if(tsk.status === false) return;
             const taskOnPage = document.createElement("article");
-            const h2 = document.createElement("h2");
-            h2.textContent = tsk.title;
-            taskOnPage.append(h2)
-            document.querySelector(".content").append(taskOnPage);
+            taskOnPage.className = "task";
+            taskOnPage.innerHTML = `<input type="checkbox" name="taskStatus" id="${tsk.id}">
+                        <div class="taskTextContent">
+                            <h3 class="taskHeader">${tsk.title}</h3>
+                            <p class="taskDesc">${tsk.description}</p>
+                        </div>`
+            document.getElementById("task-container").append(taskOnPage);
         }
 
     }
 
     init() {
+        this.storage.getAll();
+        this.todo.addTask("Vety loooooooong titleeeeee!", "I love chiken and lava", "04.11.2025", "Void");
+        this.storage.addAll();
         this.updatePage();
+        this.addEvents();
+
     }
 }
