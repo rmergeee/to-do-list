a089a5e5b1309daa2d33a0d967105144e0aa7841
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-10-04 19:44:26 +0300

    feat: reworked the method of adding tasks to the page:
    - Now, when adding a task to the page, events are immediately attached
    to it. - When the status of a task changes, the task in localStorage
    immediately changes and disappears from the page.

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,47 +1,44 @@
 import ToDoList from "../core/ToDoList";
-import LocalStorageRepo from "../storage/LocalStorageRepo"
+import LocalStorageRepo from "../storage/LocalStorageRepo";
 
 export default class App {
+  constructor() {
+    this.todo = new ToDoList();
+    this.storage = new LocalStorageRepo(this.todo);
+  }
 
-    constructor() {
-        this.todo = new ToDoList();
-        this.storage = new LocalStorageRepo(this.todo);
-    }
+  uploadTaskToPage() {
+    const self = this;
 
-    addEvents() {
-        const checkboxes = document.querySelectorAll("input[name=taskStatus]");
-        checkboxes.forEach((checkbox) => {
-            checkbox.addEventListener("change", () => {
-                this.todo.changeStatus(checkbox.id);
-                console.log(this.todo.taskStorage.find(item => item.id === checkbox.id))
-            })
-
+    this.todo.taskStorage.forEach((tsk) => {
+      if (tsk.status === true) {
+        let taskOnPage = document.createElement("article");
+        taskOnPage.innerHTML = `<input type="checkbox" name="taskStatus" id="${tsk.id}">
+         <div class="taskTextContent">
+           <h3 class="taskHeader">${tsk.title}</h3>
+           <p class="taskDesc">${tsk.description}</p>
+         </div>`;
+        taskOnPage.className = "task";
+        document.getElementById("task-container").append(taskOnPage);
+        document.getElementById(tsk.id).addEventListener("change", function () {
+          if (this.checked) {
+            tsk.status = false;
+            console.log("Checkbox is checked!");
+          } else {
+            tsk.status = true;
+            console.log("Checkbox is unchecked!");
+          }
+          if (tsk.status === false) {
+            document.getElementById(tsk.id).parentElement.remove();
+          }
+          self.storage.setTaskById(tsk.id, tsk);
         });
+      }
+    });
+  }
 
-    }
-
-    updatePage() {
-        this.storage.getAll();
-        for (let tsk of this.todo.taskStorage) {
-            if(tsk.status === false) return;
-            const taskOnPage = document.createElement("article");
-            taskOnPage.className = "task";
-            taskOnPage.innerHTML = `<input type="checkbox" name="taskStatus" id="${tsk.id}">
-                        <div class="taskTextContent">
-                            <h3 class="taskHeader">${tsk.title}</h3>
-                            <p class="taskDesc">${tsk.description}</p>
-                        </div>`
-            document.getElementById("task-container").append(taskOnPage);
-        }
-
-    }
-
-    init() {
-        this.storage.getAll();
-        this.todo.addTask("Vety loooooooong titleeeeee!", "I love chiken and lava", "04.11.2025", "Void");
-        this.storage.addAll();
-        this.updatePage();
-        this.addEvents();
-
-    }
+  init() {
+    this.storage.getAllTasksToVirtualStorage();
+    this.uploadTaskToPage();
+  }
 }
