bfac051580d19daafa3a7b8de10060c5a65e9440
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-10-20 20:18:53 +0300

    style: change indents

--- src/core/Task.js
+++ src/core/Task.js
@@ -1,12 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
 
 export default class Task {
-  constructor(title, description, deadline, project = null) {
-    this.id = uuidv4();
-    this.title = title;
-    this.description = description;
-    this.deadline = deadline;
-    this.status = true;
-    this.project = project;
-  }
+    constructor(title, description, deadline, project = null) {
+        this.id = uuidv4();
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.status = true;
+        this.project = project;
+    }
 }
--- src/core/ToDoList.js
+++ src/core/ToDoList.js
@@ -1,22 +1,16 @@
 import Task from "./Task";
 
 export default class ToDoList {
-  constructor() {
-    this.taskStorage = [
-      // new Task(
-      //   "This is example",
-      //   "This task is example. Please complete me.",
-      //   "04.10.2025",
-      // ),
-    ];
-  }
+    constructor() {
+        this.taskStorage = [];
+    }
 
-  addTask(title, description, deadline, project) {
-    this.taskStorage.push(new Task(title, description, deadline, project));
-  }
+    addTask(title, description, deadline, project) {
+        this.taskStorage.push(new Task(title, description, deadline, project));
+    }
 
-  changeStatus(id) {
-    const task = this.taskStorage.find((item) => item.id === id);
-    task.status = task.status ? false : true;
-  }
+    changeStatus(id) {
+        const task = this.taskStorage.find((item) => item.id === id);
+        task.status = task.status ? false : true;
+    }
 }
