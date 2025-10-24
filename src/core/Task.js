719227260e7120602a0b912b2495b7d3ea052a42
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-10-24 14:19:04 +0300

    change the status property to isCompleted

--- src/core/Task.js
+++ src/core/Task.js
@@ -1,12 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
 
 export default class Task {
-    constructor(title, description, deadline, project = null) {
+    constructor(title, description, deadline, priority, project = null) {
         this.id = uuidv4();
         this.title = title;
         this.description = description;
         this.deadline = deadline;
-        this.status = true;
+        this.isCompleted = false;
+        this.priority = priority;
         this.project = project;
     }
 }
