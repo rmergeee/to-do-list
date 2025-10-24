956bbeceedb8e0d2c649d847892e1b6335b0fee2
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-10-24 14:18:20 +0300

    Extend ToDoList
    - Add method updateTask(id, updates)
    - Add method deleteTask(id)
    - Add method getTask(id)
    - Add filters: getTasksByStatus(), getTasksByProject(),
      getTasksByPriority()

--- src/core/ToDoList.js
+++ src/core/ToDoList.js
@@ -5,12 +5,46 @@ export default class ToDoList {
         this.taskStorage = [];
     }
 
-    addTask(title, description, deadline, project) {
-        this.taskStorage.push(new Task(title, description, deadline, project));
+    addTask(title, description, deadline, priority, project) {
+        this.taskStorage.push(new Task(title, description, deadline, priority, project));
     }
 
-    changeStatus(id) {
-        const task = this.taskStorage.find((item) => item.id === id);
-        task.status = task.status ? false : true;
+    updateTask(id, updates) {
+        const task = this.getTask(id);
+        if (task) {
+            task.title = updates.title ?? task.title;
+            task.description = updates.description ?? task.description;
+            task.deadline = updates.deadline ?? task.deadline;
+            task.priority = updates.priority ?? task.priority;
+        }
+        return null;
+    }
+
+    getTask(id) {
+        return this.taskStorage.find((task) => task.id === id);
+    }
+
+    deleteTask(id) {
+        const taskIndex = this.taskStorage.findIndex((task) => task.id === id);
+        if (taskIndex !== -1) {
+            this.taskStorage.splice(taskIndex, 1);
+        }
+    }
+
+    changeTaskStatus(id) {
+        const task = this.taskStorage.find((task) => task.id === id);
+        task.isCompleted = task.isCompleted ? false : true;
+    }
+
+    getTasksByStatus(isCompleted) {
+        return this.taskStorage.filter((task) => task.isCompleted === isCompleted);
+    }
+
+    getTasksByProject(project) {
+        return this.taskStorage.filter((task) => task.project === project);
+    }
+
+    getTasksByPriority(priority) {
+        return this.taskStorage.filter((task) => task.priority === priority);
     }
 }
